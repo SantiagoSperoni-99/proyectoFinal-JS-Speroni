0d1bba2daae97da90b978330bff911b8c33e9aec
Santiago Speroni <speronisantiago@gmail.com>
2022-06-15 23:50:42 -0300

    agregando librerias

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let userName = prompt("Ingresá tu nombre");
 let userJob = prompt('En la opcion de cago se debe elegir:\n 1 - Gerente.\n 2 - Vendedor.\n 3 - Repartidor.\n ESCRIBIR COMO APARECE EN LAS OPCIONES. ');
+const botonAlert = document.getElementById('botonAlert');
 
-
-while (userJob == 'Repartidor') {
+/* while (userJob == 'Repartidor') {
 
     switch (userJob) {
         case 'Vendedor':
@@ -20,3 +20,15 @@ while (userJob == 'Repartidor') {
 
     userJob = prompt('En la opcion de cago se debe elegir:\n 1 - Gerente.\n 2 - Vendedor.\n 3 - Repartidor.\n ESCRIBIR COMO APARECE EN LAS OPCIONES. ');
 };
+ */
+
+botonAlert.addEventListener('click', () => {
+    Swal.fire({
+        title: 'Bienvenido/a',
+        text: 'Gracias por visitarnos, por el momento no se encuentran disponibles las acciones de Ingreso y Egreso de mercadería. Estamos trabajando en ello !',
+        icon: 'error',
+        confirmButtonText: 'Volver'
+    })
+})
+
+//ACÁ QUIERO QUE AL TOCAR LOS BOTONES DE SUMAR O RESTAR STOCK SALTE UN MENSAJE CON LA LIBRERÍA sweetalert PERO NO ME APARECE
--- js/stock.js
+++ js/stock.js
@@ -1,9 +1,45 @@
+/* let Clean = document.querySelector('.remover');
+let listaArt = document.getElementById('.tablaArt');
+let userJob = document.getElementById('.userJob');
+let noMostrar = document.getElementById('.userJob').values(3);
+const boton = document.getElementById('btnAceptar');
+
+
+boton.onclick = () => {
+
+    if (userJob === noMostrar) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            footer: '<a href="">Why do I have this issue?</a>'
+        })
+    } else {
+        Clean.remove();
+    }
+
+}; */
+
+//Clean.remove()
+
+/* const boton = document.getElementById('btnAceptar');
+
+boton.addEventListener('click', () => {
+    Swal.fire({
+        title: 'Bienvenido/a',
+        Text: 'te damos la bienvenida a nuestro sitio',
+        icon: 'error',
+        confirmButtonText: 'Volver'
+    })
+}) */
+
 let Clean = document.querySelector('.remover');
-let listaArt = document.getElementById('.tablaArt')
+const boton = document.getElementById('btnAceptar');
 
+boton.onclick = () => {
+    Clean.remove();
+}
 
+let listaArt = document.getElementById('.tablaArt');
 
-btnAceptar.onclick = () => {
-
-    Clean.remove()
-};
+// EN ESTA PARTE DEL CODIGO LO QUE QUIERO ES QUE AL MOSTRAR LA CARD PARA COMPLETAR NOMBRE Y TRABAJO, AL CLICKEAR ACEPTAR SE BORRE ESTA CARD Y MUESTRE LA LISTA DE PRODUCTOS, PERO UNICAMENTE SI NO ES REPARTIDOR. PROBE DE MIL FORMAS Y NO ME ESTÁ SALIENDO
